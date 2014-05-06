7c732b2546dcc1bcae4deac27e2e233d00e2e6dd
Germán Bobr <german@germanbobr.com.ar>
2014-05-06 01:40:07 -0300

    Header de imperium
    Agregado el header de imperium

--- application/views/header.php
+++ application/views/header.php
@@ -4,4 +4,5 @@
 		<link rel="stylesheet" href="/table-styles.css" />
 		<link rel="stylesheet" href="/isamp.css" />
 	</head>
-	<body>
+	<body style="margin:0px;">
+	<iframe class="imphead"  width="100%" scrolling="No" height="29" frameborder="0" src="http://www.imperiumgames.com.ar/head/head.php"></iframe>
